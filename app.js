84db8eda59cea2748bfa8d228c3c1f6e400050c8
Cave Smith <dracothedragon9292@gmail.com>
2026-09-09 16:55:21 -0600

    added swagger

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import router from './src/router.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' with { type: 'json' };
 
 const app = express();
 
@@ -8,6 +10,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   return res.status(200).json({ message: 'Server is running' });
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(router);
 
--- src/router.js
+++ src/router.js
@@ -3,7 +3,82 @@ import { getBooksHandler, getBookByIdHandler } from './controllers/books.js';
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       additionalProperties: true
+ *       description: A book record returned by the database
+ *     Error:
+ *       type: object
+ *       required:
+ *         - message
+ *       properties:
+ *         message:
+ *           type: string
+ */
+
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get all books
+ *     responses:
+ *       '200':
+ *         description: A list of books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.get('/books', getBooksHandler);
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get a book by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the book to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: The requested book
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       '404':
+ *         description: Book not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.get('/books/:id', getBookByIdHandler);
 
 export default router;
